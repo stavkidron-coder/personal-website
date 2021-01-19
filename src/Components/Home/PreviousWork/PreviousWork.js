9a56156dd089017389fcd16c5e2f8bb02d9fcd12
Stav Kidron <kidronstav@gmail.com>
2021-01-19 01:02:31 -0600

    added videos of SONGWRITER and Locavori

--- src/Components/Home/PreviousWork/PreviousWork.js
+++ src/Components/Home/PreviousWork/PreviousWork.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import songwriterImg from '../../../Images/songwriterViewSong.png';
 import movieSagasImg from '../../../Images/movieSagas.png';
 import reactGallery from '../../../Images/react-gallery.png';
+import ReactPlayer from "react-player";
 import '../PreviousWork/PreviousWork.css'
 import { Container, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button, Row, Col, CardFooter } from 'reactstrap';
 
@@ -26,7 +27,7 @@ class Work extends Component {
                     <h2 className="prevWorkTitle">WHAT I'VE DONE</h2>
                     <hr className="prevWorkHr"/>
                     <br/>
-                    <Row>
+                    {/* <Row>
                         <Col xs="12" sm="6" lg="4" data-aos="zoom-in-up" data-aos-duration="400">
                             <Card className="projectCards">
                                 <CardImg src={songwriterImg} alt="SONGWRITER" />
@@ -71,6 +72,68 @@ class Work extends Component {
                                 </CardFooter>
                             </Card>
                         </Col>
+                    </Row> */}
+
+                    <Row>
+                        <Col xs="12" xl="8">
+                            <ReactPlayer url="https://youtu.be/Vp5uYotTg4U" className="video-left"/>
+                        </Col>
+                        <Col xs="12" xl="4">
+                            <div className="songwriter-description">
+                                <h3 className="prevWorkTitle songwriterDescriptionTitle">
+                                    SONGWRITER
+                                </h3>
+
+                                <div className="workDescTextContainer">
+                                    <p className="workDescriptionText">
+                                        SONGWRITER is a web app that allows artists to document the songwriting
+                                        and pre-production process and store songs securely online so that they don’t have to
+                                        worry about losing work ever again.
+                                    </p>
+                                    <h5>
+                                        Technologies Used:
+                                    </h5>
+                                    <p>
+                                        React, Redux, Node.js, PostgreSQL, Bootstrap
+                                    </p>
+                                </div>
+                                <div className="descriptionFooter">
+                                    <Button color="primary" onClick={this.songwriterBtn}>View App</Button>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+
+                    <br/>
+                    <hr className="prevWorkHr"/>
+                    <br/>
+                    
+                    <Row>
+                    <Col xs="12" xl="8">
+                            <ReactPlayer url="https://youtu.be/P7m1ms_7cPs" className="video-left"/>
+                        </Col>
+                        <Col xs="12" xl="4">
+                            <div className="songwriter-description">
+                                <h3 className="prevWorkTitle locavoriDescriptionTitle">
+                                    Locavori
+                                </h3>
+
+                                <div className="workDescTextContainer">
+                                    <p className="workDescriptionText">
+                                    Locavori is a full-stack web application that gives consumers who love to
+                                    buy local (Locavores) a platform to find local artisans all around the world
+                                    as well as allows local artisans to create a profile and advertise their products.
+                                    </p>
+                                    <h5>
+                                        Technologies Used:
+                                    </h5>
+                                    <p>
+                                        React, Redux, Node.js, PostgreSQL, Bootstrap, Google Maps APIs (Maps, Distance Matrix, Places, Geolocation)
+                                    </p>
+                                </div>
+                            </div>
+                        </Col>
+                        
                     </Row>
                 </Container>
             </div>
